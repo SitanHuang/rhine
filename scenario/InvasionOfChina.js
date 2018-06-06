abba53ce9d586cab464f7e3f89a3d710c9686a21
Sitan Huang(黄斯坦) <978494543.qq54@gmail.com>
2018-06-05 21:54:54 -0700

    Update InvasionOfChina.js

--- scenario/InvasionOfChina.js
+++ scenario/InvasionOfChina.js
@@ -120,21 +120,21 @@ RR  RR  M    RR                 R      MMMM   @@@@@@@@@@@|
 MAP_DATA = InvasionOfChinaTerrains;
 pt(25, 43).prov.slots = Array(25).fill('F'); // Nanjing
 pt(30, 35).prov.slots = Array(15).fill('F'); // Wuhan
-pt(33, 22).prov.slots = Array(15).fill('F'); // Chongqing
+pt(33, 22).prov.slots = Array(10).fill('F'); // Chongqing
 
 
 let p2 = new Player();
 p2.color = 'rgba(140, 193, 226, 0.25)'
 p2.retreatable = 30;
 p2.manpower = Math.floor(Math.random() * 50000 + 10000000);
-p2.light = 100;
-p2.heavy = 200;
+p2.light = 0;
+p2.heavy = 0;
 p2.factoryInLight = 40;
 let p1 = new Player();
 p1.color = randomColor({alpha: 0.2, format: 'rgba', hue: 'red'})
 p1.manpower = Math.floor(Math.random() * 50000 + 5000000);
-p1.light = 300;
-p1.heavy = 600;
+p1.light = 400;
+p1.heavy = 700;
 p1.retreatable = 30;
 p1.factoryInLight = 22;
 p1.savedTemplates = [{"troop":8000,"light":20,"heavy":5,"defaultName":"Infantry Division"},{"troop":23000,"light":5,"heavy":12,"defaultName":"T12 Heavy Tank Division"},{"troop":20000,"light":25,"heavy":10,"defaultName":"T49 Armored Infantry Division"},{"troop":10000,"light":5,"heavy":6,"defaultName":"Light Motorized Mountaineer Division"},{"troop":17000,"light":38,"heavy":6,"defaultName":"T67 Armored Infantry Mountaineer Division"},{"troop":39000,"light":25,"heavy":10,"defaultName":"T918 Heavy Armored Infantry Division"},{"troop":23000,"light":10,"heavy":12,"defaultName":"T24 Heavy Tank Division"},{"troop":23000,"light":20,"heavy":3,"defaultName":"T42 Heavy Calvary Mountaineer Division"}].map(x => {let t = new Template(x.troop, x.light, x.heavy);t.defaultName = x.defaultName;return t});
@@ -146,17 +146,17 @@ MAP_DATA.forEach((x, row) => (x.forEach((v, col) => {
     PORTS.push(pt(row, col));
   if (v.pt.adjacentNotToPlayer(v.pt.owner) > 0 || v.terrain == 'U' || v.terrain == 'P')
     if (v.owner == 0)
-      v.divisions = Array(v.terrain == 'P' ? 2 : 5).fill(0).map(() => (new Division(v.owner, 'Infantry Division', pt(row, col), new Template(7000, 17, 5))))
+      v.divisions = Array(v.terrain == 'P' ? 5 : 3).fill(0).map(() => (new Division(v.owner, 'Infantry Division', pt(row, col), new Template(7000, 17, 5))))
     else
       v.divisions = Array(v.terrain == 'U' ? 2 : 5).fill(0).map(() => (new Division(v.owner, 'Infantry Division', pt(row, col), new Template(10000, 20, 15))))
 })));
 PORTS = PORTS.sort(() => (Math.random() - 0.5));
 // Shanghai
 pt(27, 46).prov.divisions = Array(35).fill(0).map(() => (new Division(1, 'Infantry Division', pt(27, 46), new Template(10000, 20, 15))))
-  .concat(Array(10).fill(0).map(() => (new Division(1, 'Tank Division', pt(27, 46), new Template(10000, 10, 10)))));
+  .concat(Array(10).fill(0).map(() => (new Division(1, 'Tank Division', pt(27, 46), new Template(10000, 9, 15)))));
 // Nanjing
-pt(25, 43).prov.divisions = Array(7).fill(0).map(() => {
+pt(25, 43).prov.divisions = Array(5).fill(0).map(() => {
   let d = new Division(0, 'German Infantry Division', pt(25, 43), new Template(19000, 20, 35))
-  d.skill = 4;
+  d.skill = 0.9;
   return d;
 })
