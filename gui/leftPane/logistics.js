bea7fedc9c10f83466a61b85fc5d1b0b4216504e
Sitan Huang(黄斯坦) <978494543.qq54@gmail.com>
2018-11-10 09:22:28 -0800

    Add files via upload

--- gui/leftPane/logistics.js
+++ gui/leftPane/logistics.js
@@ -291,6 +291,38 @@ function toggleAverageMoraleOnClick(button) {
     });
   }
 }
+function toggleAverageEntrenchOnClick(button) {
+  if (button.showing) {
+    button.className = '';
+    button.showing = false;
+    repaintCanvas();
+  } else {
+    button.className = 'active';
+    button.showing = true;
+    let oldCallback = colCallback;
+    colCallback = td => {
+    }
+    repaintCanvas(function (td) {
+      let pt = td.pt;
+      let prov = pt.prov;
+      td.style.cursor = 'pointer';
+      if (pt.owner == currentPlayer) {
+        td.style.backgroundColor = 'white';
+        if(prov.divisions.length > 0) {
+          let num = 0;
+          prov.divisions.forEach(x => {num += x.entrench});
+          num = num / prov.divisions.length;
+          td.style.backgroundColor = getColorFromPercentage(num / 2, [{"pct":0,"color":{"r":180,"g":0,"b":0}},{"pct":0.5,"color":{"r":180,"g":180,"b":0}},{"pct":1,"color":{"r":32,"g":158,"b":32}}]);
+          let number = document.createElement('number');
+          number.innerText = (num * 10).round();
+          td.appendChild(number)
+        }
+      } else {
+        td.style.backgroundColor = 'grey';
+      }
+    });
+  }
+}
 
 function toggleSuppliesOnClick(button) {
   if (button.showing) {
@@ -431,6 +463,7 @@ function updateLogistics() {
   <button onclick="toggleAverageStrengthHPOnClick(this)">Toggle Average Strength View (By HP)</button><br><br>
   <button onclick="toggleAverageSkillOnClick(this)">Toggle Average Skill View</button><br><br>
   <button onclick="toggleAverageMoraleOnClick(this)">Toggle Average Morale View</button><br><br>
+  <button onclick="toggleAverageEntrenchOnClick(this)">Toggle Average Entrench View</button><br><br>
   <br><br><br>
   <p>V1.0.2</p>
   `;
