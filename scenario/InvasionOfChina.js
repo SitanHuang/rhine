78d08a51a3d7d00c7993babffde9bd1a3932bf6b
Sitan Huang(黄斯坦) <978494543.qq54@gmail.com>
2019-02-10 12:31:48 -0800

    Update InvasionOfChina.js

--- scenario/InvasionOfChina.js
+++ scenario/InvasionOfChina.js
@@ -147,7 +147,7 @@ MAP_DATA.forEach((x, row) => (x.forEach((v, col) => {
   if (v.terrain == '@') return;
   if (v.terrain == 'P')
     PORTS.push(pt(row, col));
-  if (v.pt.adjacentNotToPlayer(v.pt.owner) > 0 || v.terrain == 'U' || v.terrain == 'P')
+  if (v.pt.adjacentNotToPlayer(v.pt.owner) > 0 || v.terrain == 'P')
     if (v.owner == 0)
       v.divisions = Array(v.terrain == 'P' || v.terrain == 'D' ? 1 : Math.ceil(Math.random() * 9 + 3)).fill(0).map(() => (new Division(v.owner, `Regiment, ${((++divisions)/2).floor()}th Division`, pt(row, col), new Template(Math.random() > 0.6 ? 7000 : 4000, 7, 1))))
     else
