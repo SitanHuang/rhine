95e8169bdbf6f177dfa99058ed301cbacb284ef6
Sitan Huang(黄斯坦) <978494543.qq54@gmail.com>
2020-10-12 11:30:26 -0700

    Update WWII.js

--- scenario/WWII.js
+++ scenario/WWII.js
@@ -148,14 +148,15 @@ function loadWWIIScenario() {
   PLAYERS[0].casualties = 0;
   PLAYERS[0].divisionMen = 0;
   PLAYERS[0].factories = 0;
-  PLAYERS[0].growthRate = 149187/47080000; // 0.0029563933
+  //PLAYERS[0].growthRate = 149187/47080000; // 0.0029563933
+  PLAYERS[0].growthRate = 149187/47080000 + 0.003;
   PLAYERS[1].color = "rgba(96, 96, 96, 0.2)";
   PLAYERS[1].manpower = 5500000;
   PLAYERS[1].casualties = 0;
   PLAYERS[1].divisionMen = 0;
   PLAYERS[1].factories = 0;
   // PLAYERS[1].growthRate = 482790/67349000; // 0.0071684806
-  PLAYERS[1].growthRate = 0.003;
+  PLAYERS[1].growthRate = 0.006;
 
   PLAYERS[0].light = 1000;
   PLAYERS[0].heavy = 2000;
