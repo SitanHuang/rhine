a83adeebb673d21a110caf914cead954c8d4ff86
SitanHuang <978494543.qq54@gmail.com>
2020-10-16 08:49:20 -0700

    fix pathfind over no supply areas

--- core/pathfinding/pathfinding.js
+++ core/pathfinding/pathfinding.js
@@ -14,6 +14,8 @@ function unit_pathfind(start, end) {
       }
       if (col.terrain == '@')
         return 0;
+      if (col.supply && col.supply <= 0.05)
+        return 0;
       return 1 / terrain.movement * 2 + 1 + w;
     })
   }));
