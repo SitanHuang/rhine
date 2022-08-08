c7ae6ed7a3490fbcce31ab924f83fe07e81c69ca
SitanHuang <978494543.qq54@gmail.com>
2022-08-07 22:59:19 -0700

    supply cap

--- scenario/1936.js
+++ scenario/1936.js
@@ -200,7 +200,7 @@ function load1936Scenario() {
   p3.light = -13000;
   p3.factories = 0;
   //p3.constructionPoints = 6500;
-  p3.heavy = -8000;
+  p3.heavy = -7000;
   p3.factoryInLight = 1;
   p3.generals = {"Generalissimo":{"Chiang Kai-Shek":{"desc":"President, Republic Of China","path":"scenario/ChineseGenerals/GeneralissimoChiangKaiShek.jpg","mod":{"o":1.1,"b":1.1,"s":1,"e":1.1},"selected":true},"Game Founder":{"desc":"Programmer","path":"scenario/ChineseGenerals/GeneralissimoSitanHuang.jpg","mod":{"o":0.8,"b":1.4,"s":1.2,"e":0.8},"selected":false}},"Field Marshal":{"Hu Zong-Nan":{"desc":"Eagle of the Northwest","path":"scenario/ChineseGenerals/FieldMarshalHuZongNan.jpg","mod":{"o":1.3,"b":0.9,"s":0.8,"e":0.9},"selected":true},"Li Zong-ren":{"desc":"Vice President, Republic Of China","path":"scenario/ChineseGenerals/FieldMarshalLiZongRen.jpg","mod":{"o":1,"b":1.3,"s":0.8,"e":1},"selected":false},"Gao Zhi-hang":{"desc":"Modern Warfare Expert","mod":{"o":0.8,"b":1.3,"s":1.3,"e":0.7},"path":"scenario/ChineseGenerals/FieldMarshalGaoZhihang.jpg","selected":false},"Zhu De":{"desc":"Pioneers of Liberation","mod":{"o":1.3,"b":1.2,"s":0.9,"e":0.8},"path":"scenario/ChineseGenerals/FieldMarshalZhuDe.jpg"},"Xiao Yi-Su":{"desc":"Minister of National Defense, Republic Of China","mod":{"o":1,"b":0.8,"s":0.7,"e":1.5},"path":"scenario/ChineseGenerals/FieldMarshalXiaoYiSu.jpg","selected":true}}};
   p3.sumAllGeneralTraits;
@@ -289,7 +289,7 @@ function load1936Scenario() {
         });
         v.divisions.map(x => {x.skill = 4;x.morale = 2;});
       } else if (v.owner == 2 && (v.terrain == 'P' || v.terrain == 'U')) {
-        let type = Math.random() < 0.5 ? soviet : (Math.random() < 0.05 ? sovietMech : sovietMilitia);
+        let type = Math.random() < 0.8 ? soviet : (Math.random() < 0.05 ? sovietMech : sovietMilitia);
         v.divisions = Array((Math.random() * 2).round() + 4).fill(0).map(() => (new Division(v.owner, ++soviet_i + 'th ' + type.defaultName, pt(row, col), type)));
         v.divisions.map(x => {x.skill = 0.3 + Math.random();x.men = (Math.random() * 0.5 + 0.3) * x.template.troop;});
       } else if (v.owner == 3 && (v.terrain == 'P' || v.terrain == 'U')) {
@@ -376,7 +376,7 @@ function trigger_german_ai_attack1936(num) {
       PLAYERS[2].heavy += 4000;
       PLAYERS[1].light = -5000;
       PLAYERS[1].heavy = -8000;
-      PLAYERS[1].growthRate /= 2;
+      PLAYERS[1].growthRate /= 3;
       PLAYERS[2].constructionPoints += 550 * 30;
       PLAYERS[1].constructionPoints = -550 * 50;
       PLAYERS[0].constructionPoints += 550 * 30 + 150*20;
